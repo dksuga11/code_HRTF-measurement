817fa3fe0da874cd5e3eeb730fb9d6897774af17
daiki sugawara <dksuga3652@gmail.com>
2023-07-28 10:31:38 +0900

    関数の呼び出し位置等を修正

--- make_transaural.py
+++ make_transaural.py
@@ -6,7 +6,7 @@ import os
 import util
 
 
-def make_transaural_filter(H_ll, H_rl, H_lr, H_rr):
+def make_inverse_filter(H_ll, H_rl, H_lr, H_rr):
     """トランスオーラルの生成に必要なフィルタを生成する
     Args:
         H_ll(ndarray): 左スピーカから左耳のまでのHRTF
@@ -44,14 +44,13 @@ def make_transaural_sound(sig, H_ll, H_rl, H_lr, H_rr):
     sig_l = sig.T[0]
     sig_r = sig.T[1]
 
-    G_ll, G_rl, G_lr, G_rr = make_transaural_filter(H_ll, H_rl, H_lr, H_rr)
-    g_ll = np.real(np.fft.irfft(G_ll))
-    g_rl = np.real(np.fft.irfft(G_rl))
-    g_lr = np.real(np.fft.irfft(G_lr))
-    g_rr = np.real(np.fft.irfft(G_rr))
+    h_ll = np.fft.irfft(H_ll)
+    h_rl = np.fft.irfft(H_rl)
+    h_lr = np.fft.irfft(H_lr)
+    h_rr = np.fft.irfft(H_rr)
 
-    tranceaural_l = sp.fftconvolve(sig_l, g_ll) + sp.fftconvolve(sig_r, g_rl)
-    tranceaural_r = sp.fftconvolve(sig_l, g_lr) + sp.fftconvolve(sig_r, g_rr)
+    tranceaural_l = sp.fftconvolve(sig_l, h_ll) + sp.fftconvolve(sig_r, h_rl)
+    tranceaural_r = sp.fftconvolve(sig_l, h_lr) + sp.fftconvolve(sig_r, h_rr)
 
     tranceaural = np.array([tranceaural_l, tranceaural_r]).T
 
@@ -65,7 +64,9 @@ if __name__ == "__main__":
     例 -> python make_tranceaural.py folderpath original.wav
     出力:
     - .wav
-        - original.wavをトランスオーラルに変換した音源
+        - original_Hs.wav: イヤホンで聞いた時に、地下室の残響が再現されてるはずの音．音源とインパルス応答の畳込み
+        - original_GHs.wav: sigに戻ってるはずの音．Hsとインパルス応答の逆行列のから求めたフィルタの畳み込み
+        - original_Gs.wav: 地下室で聞いた時に、イヤホンで聞いたような音になるはずの音．音源とインパルス応答の逆行列のから求めたフィルタの畳み込み
     注意:
     - フォルダの中身（例のfoldrpath）には，左右のスピーカーごとに{sp-l}というフォルダを作成し，その中に左右のHRTFを{HRTF-l.npy}という名前で保存する
     """
@@ -82,9 +83,31 @@ if __name__ == "__main__":
     H_rr = np.load(f"{folderpath}/sp-r/HRTF-r.npy")
     sig, fs = sf.read(filepath_sound)
 
-    tranceaural = make_transaural_sound(sig, H_ll, H_rl, H_lr, H_rr)
+    G_ll, G_rl, G_lr, G_rr = make_inverse_filter(H_ll, H_rl, H_lr, H_rr)
+    Hs = make_transaural_sound(
+        sig, H_ll, H_rl, H_lr, H_rr
+    )  # イヤホンで聞いた時に、地下室の残響が再現されてるはず
+    GHs = make_transaural_sound(Hs, G_ll, G_rl, G_lr, G_rr)  # sigに戻ってるはず
+    Gs = make_transaural_sound(
+        sig, G_ll, G_rl, G_lr, G_rr
+    )  # 地下室で聞いた時に、イヤホンで聞いたような音になるはず
+    print(G_ll * H_ll + G_lr * H_rl)
+    print("\n")
+    print(G_ll * H_rl + G_rl * H_rr)
 
     filename, _ = util.get_fileinfo(filepath_sound)
     sf.write(
-        file=f"{folderpath}/{filename}_tranceaural.wav", data=tranceaural, samplerate=fs
+        file=f"{folderpath}/{filename}_Hs.wav",
+        data=Hs,
+        samplerate=fs,
+    )
+    sf.write(
+        file=f"{folderpath}/{filename}_GHs.wav",
+        data=GHs,
+        samplerate=fs,
+    )
+    sf.write(
+        file=f"{folderpath}/{filename}_Gs.wav",
+        data=Gs,
+        samplerate=fs,
     )
